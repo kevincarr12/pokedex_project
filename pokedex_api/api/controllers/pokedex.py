bd961cf3c7ac3eefd907d219077ee87cf86f79cd
kevincarr12 <kevinc1295@gmail.com>
2024-03-04 20:21:23 -0500

    Added CRUD for Pokedex

--- pokedex_api/api/controllers/pokedex.py
+++ pokedex_api/api/controllers/pokedex.py
@@ -1,4 +1,3 @@
-from flask import jsonify
 from api.model.models import db, Pokedex
 
 class pokedexController():
@@ -11,5 +10,31 @@ class pokedexController():
 	def getPokedexTrainer(id_trainer):
 		session: sqlalchemy.orm.scoping.scoped_session = db.session
 		pokedex = session.query(Pokedex).filter(Pokedex.id_trainer.in_([id_trainer])).all()
+
+		return pokedex
+
+	def addPokedex(id_trainer, id_pokemon, catch_date):
+		session: sqlalchemy.orm.scoping.scoped_session = db.session
+		pokedex = Pokedex()
+		pokedex.id_trainer = id_trainer
+		pokedex.id_pokemon = id_pokemon
+		pokedex.catch_date = catch_date
+		db.session.add(pokedex)
+		db.session.commit()
+
+		return pokedex
+
+	def updatePokedex(id_pokedex, id_trainer, id_pokemon, catch_date):
+		session: sqlalchemy.orm.scoping.scoped_session = db.session
+		pokedex = session.query(Pokedex).filter(Pokedex.id_pokedex == id_pokedex).\
+				update({'id_trainer':id_trainer,'id_pokemon':id_pokemon,'catch_date':catch_date})
+		db.session.commit()
+
+		return pokedex
+
+	def deletePokedex(id_pokedex):
+		session: sqlalchemy.orm.scoping.scoped_session = db.session
+		pokedex = session.query(Pokedex).filter(Pokedex.id_pokedex == id_pokedex).delete()
+		db.session.commit()
 
 		return pokedex
--- pokedex_api/api/route/pokedex.py
+++ pokedex_api/api/route/pokedex.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from http import HTTPStatus
 from flasgger import swag_from
 from api.controllers.pokedex import pokedexController
@@ -14,7 +14,7 @@ pokedex_bp = Blueprint('pokedex', __name__, url_prefix='/pokedex')
 		}
 	}
 })
-@pokedex_bp.route('/')
+@pokedex_bp.route('/', methods=['GET'])
 def getPokedex():
 	pokedex = pokedexController
 	resultPokedex = pokedex.getPokedex()
@@ -28,9 +28,60 @@ def getPokedex():
 		}
 	}
 })
-@pokedex_bp.route('/<int:id_trainer>')
+@pokedex_bp.route('/<int:id_trainer>', methods=['GET'])
 def getPokedexTrainer(id_trainer):
 	pokedex = pokedexController
 	resultPokedex = pokedex.getPokedexTrainer(id_trainer)
 	print(resultPokedex)
 	return pokedexs_schema.dump(resultPokedex)
+
+@swag_from({
+	'responses': {
+		HTTPStatus.OK.value: {
+			'description':'Add a new Pokedex entry',
+			'schema':pokedexSchema
+		}
+	}
+})
+@pokedex_bp.route('/', methods=['POST'], strict_slashes=False)
+def addPokedex():
+	id_trainer = request.json['id_trainer']
+	id_pokemon = request.json['id_pokemon']
+	catch_date = request.json['catch_date']
+	pokedex = pokedexController
+	newPokedex = pokedex.addPokedex(id_trainer, id_pokemon, catch_date)
+
+	return pokedex_schema.dump(newPokedex), 201
+
+@swag_from({
+	'responses': {
+		HTTPStatus.OK.value: {
+			'description':'Update an existing Pokedex entry',
+			'schema':pokedexSchema
+		}
+	}
+})
+@pokedex_bp.route('/<int:id_pokedex>', methods=['PUT'], strict_slashes=False)
+def updatePokedex(id_pokedex):
+	id_trainer = request.json['id_trainer']
+	id_pokemon = request.json['id_pokemon']
+	catch_date = request.json['catch_date']
+	pokedex = pokedexController
+	updatePokedex = pokedex.updatePokedex(id_pokedex, id_trainer, id_pokemon, catch_date)
+
+	return pokedex_schema.dump(updatePokedex), 200
+
+@swag_from({
+	'responses': {
+		HTTPStatus.OK.value: {
+			'description':'Delete a Pokedex entry',
+			'schema':pokedexSchema
+		}
+	}
+})
+@pokedex_bp.route('/<int:id_pokedex>', methods=['DELETE'])
+def deletePokedex(id_pokedex):
+	pokedex = pokedexController
+	deletePokedex = pokedex.deletePokedex(id_pokedex)
+
+	return pokedex_schema.dump(deletePokedex), 200
